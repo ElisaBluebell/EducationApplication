0fe4db911e35c117264b2caf157db95451691383
ElisaBluebell <selfishracer@hotmail.com>
2023-02-06 21:14:20 +0900

    230206 ElisaBluebell

--- main_server.py
+++ main_server.py
@@ -1,62 +1,77 @@
+import datetime
+import json
+import socket
+
 import server_tool as st
 import select
 
 
 class MainServer:
     def __init__(self):
-        buffer = 1024
-        server_sock, socks = st.socket_initialize()
+        self.socks = []
+        self.s_sock = socket.socket()
+        self.ip = '10.10.21.121'
+        self.port = 9000
+        self.initialize_socket()
+        self.turn_server_on()
 
-        xml_string = st.get_database_from_url(
-            'http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI?'
-            'serviceKey=yn8uwUR3eheqowtPnA9QRTQ9i8mYGhGEetp6HDG1hMhCeH9%2BNJFN6WlIM1AzfgrZB59syoKUT1rAVveE9J6Okg%3D%3D&'
-            'searchMtNm=&'
-            'searchArNm=&'
-            'pageNo=1&'
-            'numOfRows=100')
+    # 소켓 설정 함수
+    def initialize_socket(self):
+        self.s_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s_sock.bind((self.ip, self.port))
+        self.s_sock.listen()
+        self.socks.append(self.s_sock)
+        print(f'{self.port} 포트 열림')
 
-        json_dict = st.xml_to_json(xml_string)
-        body = st.get_useful_data(json_dict, 1)
-        print(body)
-
-        self.turn_server_on(server_sock, socks, buffer)
-
-    def turn_server_on(self, server_sock, sock_list, buffer):
-        import datetime
-        import json
+    def turn_server_on(self):
         while True:
-            r_sock, dummy1, dummy2 = select.select(sock_list, [], [], 0)
+            r_sock, w_sock, e_sock = select.select(self.socks, [], [], 0)
             for s in r_sock:
-                if s == server_sock:
-                    print(s)
+                if s == self.s_sock:
                     c_sock, addr = s.accept()
-                    st.add_client_to_list(c_sock, addr, sock_list)
+                    self.socks.append(c_sock)
 
                 else:
                     try:
-                        data = s.recv(buffer).decode('utf-8')
+                        data = s.recv(1024).decode('utf-8')
                         print(f'받은 메시지> {s.getpeername()}: {data} [{datetime.datetime.now()}]')
 
                         if data:
                             try:
                                 message = eval(data)
-                                return message
-                                # data = json.dumps(['명령', '내용'])
-                                # s.send(data.encode())
+                                self.command_processor(message, s)
 
                             except TypeError:
                                 data = json.dumps(['/load_chat_again', ''])
                                 s.send(data.encode())
 
+                        # 유언을 받은 경우
                         if not data:
-                            print(f'클라이언트 {addr}이 접속을 종료했습니다.')
-                            s.close()
+                            # 시체를 안고 커넥션 로스트 함수로
+                            self.connection_lost(s)
                             continue
 
                     except ConnectionResetError:
-                        print(f'클라이언트 {addr}이 접속을 종료했습니다.')
-                        s.close()
+                        self.connection_lost(s)
                         continue
+
+    def connection_lost(self, s):
+        print(f'클라이언트 {s.getpeername()} 접속 종료')
+        s.close()
+        self.socks.remove(s)
+
+    @staticmethod
+    def command_processor(message, client_sock):
+        # 명령문과 컨텐츠 구분
+        command = message[0]
+        content = message[1]
+        pass
+
+    def send_command(self, command, content, s):
+        message = [command, content, s]
+        data = json.dumps([command, content])
+        print(f'보낸 메시지: {data} [{datetime.datetime.now()}]')
+        s.send(data.encode())
 
 
 if __name__ == "__main__":
