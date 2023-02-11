9c07d821f8c61c37618d57868abb96f0fe59343e
Myoung-eun <freshme217@naver.com>
2023-02-11 13:15:11 +0900

    Initial commit

--- 0207client.py
+++ 0207client.py
@@ -7,50 +7,70 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 import sys
-# import jason
 import datetime
 from socket import *
 import threading
 
-
-
 # ui파일 연결
 form_class = uic.loadUiType("tracking.ui")[0]
+
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        self.tabWidget.setCurrentIndex(0)
         pixmap = QPixmap("C:/Users/sw/PycharmProjects/EducationApplication/등산.png")  # 코드 3줄 ui에 이미지를 넣을 수 있음
-        pixmap = pixmap.scaled(700, 700, Qt.KeepAspectRatio, Qt.SmoothTransformation) # 코드 3줄 ui에 이미지를 넣을 수 있음
-        self.login_label.setPixmap(pixmap)                                            # 코드 3줄 ui에 이미지를 넣을 수 있음
+        pixmap = pixmap.scaled(700, 700, Qt.KeepAspectRatio, Qt.SmoothTransformation)  # 코드 3줄 ui에 이미지를 넣을 수 있음
+        self.login_label.setPixmap(pixmap)  # 코드 3줄 ui에 이미지를 넣을 수 있음
 
         self.initialize_socket()
+
+        cThread = threading.Thread(target=self.receive_message, args=(self.client_socket,))
+        cThread.start()
+
         self.Map_label.setPixmap(QPixmap('100대명산지도.png'))  # ui에 이미지를 넣을 수 있음
-        self.background_label.setPixmap(QPixmap('Background.png')) # ui에 이미지를 넣을 수 있음
-        # self.login_btn.clicked.connect(self.login) # 로그인 버튼을 누르면
-        # self.sign_up.clicked.connect(self.join_us) # 회원가입(1tab) 버튼을 누르면
-        # self.quiz_tableWidget
+        self.background_label.setPixmap(QPixmap('Background.png'))  # ui에 이미지를 넣을 수 있음
+        self.login_btn.clicked.connect(self.login)  # 로그인 버튼을 누르면
+        self.sign_up.clicked.connect(self.join_us)  # 회원가입(1tab) 버튼을 누르면
         self.register_signup_btn.clicked.connect(self.sign)  # 회원가입(2tab) 버튼을 누르면
-        # self.check_ID_btn.clicked.connect(self.check_ID)  # 아이디 중복확인을 누르면
-        # self.id_lineedit.SetText(self.id_lineedit)
+        self.sign_up.clicked.connect(self.sign_)  # 1p의 회원가입을 누르면
+        self.check_ID_btn.clicked.connect(self.check_ID)  # 아이디 중복확인을 누르면
+        self.qna_tableWidget.cellDoubleClicked.connect(self.qna)
+        self.answer_lineedit.returnPressed.connect(self.send)
+        self.quizadd_lineedit.returnPressed.connect(self.update)
+        self.o_btn.clicked.connect(self.change_o)
+        self.x_btn.clicked.connect(self.change_x)
+        self.a = ''
+        self.send_btn.clicked.connect(self.send)
+        self.score_btn.clicked.connect(self.student_score)
+
+
+        #### 문제풀이에 관한 추가 데이터 요청 버튼 <도별> ####
+        self.gangwon_addbtn.clicked.connect(self.gangwon_add)
+        self.gyeonggi_addbtn.clicked.connect(self.gyeonggi_add)
+        self.chungbuk_addbtn.clicked.connect(self.chungbuk_add)
+        self.chungnam_addbtn.clicked.connect(self.chungnam_add)
+        self.gyeongbuk_addbtn.clicked.connect(self.gyeongbuk_add)
+        self.gyeongnam_addbtn.clicked.connect(self.gyeongnam_add)
+        self.jeonbuk_addbtn.clicked.connect(self.jeonbuk_add)
+        self.jeonnam_addbtn.clicked.connect(self.jeonnam_add)
+        self.jeju_addbtn.clicked.connect(self.jeju_add)
+        self.b = ''
+        # self.show()
 
         #### 문제풀이에 관한 데이터 요청 버튼 <도별> ####
-        # self.seoul_btn.clicked.connect(self.quiz_request)
-        # self.gangwondo_btn.clicked.connect(self.quiz_request)
-        # self.chungbuk_btn.clicked.connect(self.quiz_request)
-        # self.chungnam_btn.clicked.connect(self.quiz_request)
-        # self.gyeongbuk_btn.clicked.connect(self.quiz_request)
-        # self.gyeongnam_btn.clicked.connect(self.quiz_request)
-        # self.jeonbuk_btn.clicked.connect(self.quiz_request)
-        # self.jeonnam_btn.clicked.connect(self.quiz_request)
-        # self.jeju_btn.clicked.connect(self.quiz_request)
-
+        self.gangwon_btn.clicked.connect(self.gangwon)
+        self.gyeonggi_btn.clicked.connect(self.gyeonggi)
+        self.chungbuk_btn.clicked.connect(self.chungbuk)
+        self.chungnam_btn.clicked.connect(self.chungnam)
+        self.gyeongbuk_btn.clicked.connect(self.gyeongbuk)
+        self.gyeongnam_btn.clicked.connect(self.gyeongnam)
+        self.jeonbuk_btn.clicked.connect(self.jeonbuk)
+        self.jeonnam_btn.clicked.connect(self.jeonnam)
+        self.jeju_btn.clicked.connect(self.jeju)
         # self.connectserver()
         self.show()
-
-
 
     def initialize_socket(self):
         # ip = '127.0.0.1'
@@ -59,149 +79,182 @@ class WindowClass(QMainWindow, form_class):
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client_socket.connect((ip, port))
 
-    def quiz_request(self):
-        # 버튼 객체 얻기
-        btn = self.sender() ###?
+    def gangwon_add(self):
+        self.b = '/gangwon_add'
+    def gyeonggi_add(self):
+        self.b = '/seoul_add'
+    def chungbuk_add(self):
+        self.b ='/chungbuk_add'
+    def chungnam_add(self):
+        self.b = '/chungnam_add'
+    def gyeongbuk_add(self):
+        self.b = '/gyeongbuk_add'
+    def gyeongnam_add(self):
+        self.b = '/gyeongnam_add'
+    def jeonbuk_add(self):
+        self.b = '/jeonbuk_add'
+    def jeonnam_add(self):
+        self.b = '/jeonnam_add'
+    def jeju_add(self):
+        self.b = '/jeju_add'
+    def gangwon(self):
+        self.send_command('/quize_gangwon', '')
 
-        self.quiz_name = btn.text()
-        if self.quize_name == '서울/경기':
-            self.send_commang('/quize_seoul', self.quize_name)
-        elif self.quize_name == '강원도':
-            self.send_command('/quize_gangwon', self.quize_name)
-        elif self.quize_name == '충청북도':
-            self.send_command('/quize_chungbuk', self.quize_name)
-        elif self.quize_name == '충청남도':
-            self.send_command('/quize_chungnam', self.quize_name)
-        elif self.quize_name == '경상북도':
-            self.send_command('/quize_gyeongbuk', self.quize_name)
-        elif self.quize_name == '경상남도':
-            self.send_command('/quize_gyeongnam', self.quize_name)
-        elif self.quize_name == '전라북도':
-            self.send_command('/quize_jeonbuk', self.quize_name)
-        elif self.quize_name == '전라남도':
-            self.send_command('/quize_jeonnam', self.quize_name)
-        else:
-            self.send_command('/quize_jeju', self.quize_name)
-        # 버튼 객체 얻어서 그 버튼이 어떤 도의 버튼인지 확인 후 서버에 데이터 요청해야함
+    def gyeonggi(self):
+        self.send_command('/quize_seoul', '')
 
+    def chungbuk(self):
+        self.send_command('/quize_chungbuk', '')
 
+    def chungnam(self):
+        self.send_command('/quize_chungnam', '')
+
+    def gyeongbuk(self):
+        self.send_command('/quize_gyeongbuk', '')
+
+    def gyeongnam(self):
+        self.send_command('/quize_gyeongnam', '')
+
+    def jeonbuk(self):
+        self.send_command('/quize_jeonbuk', '')
+
+    def jeonnam(self):
+        self.send_command('/quize_jeonnam', '')
+
+    def jeju(self):
+        self.send_command('/quize_jeju', '')
+
+    def qna(self):
+        self.send_command('/qna', '')
+
+    def student_score(self):
+        self.send_command('/student_score', '')
+        print('학생들 점수 보내주세요.')
+
+    def sign_(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def login(self):
+        self.id = self.teacher_id.text()
+        self.password = self.teacher_pw.text()
+        self.loginlist = [self.id, self.password]
+        if '' not in self.loginlist:
+            self.send_command('/login_teacher', self.loginlist)
+
+    def login_success(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def login_id_fail(self):
+        print("로그인에 실패했습니다.")
+        self.login_label_4.text('로그인에 실패했습니다. 아이디와 비밀번호를 확인 해 주세요')
+
+    def send_command(self, command, content):
+        msg = [command, content]
+        data = json.dumps(msg)
+        print(f'보낸 메시지: {data} [{datetime.datetime.now()}]')
+        self.client_socket.send(data.encode())
 
     def sign(self):
         list = ['/register_user', [self.combo_2.currentText(), self.name.text(), self.ID.text(), self.ps_3.text()]]
+        self.join_label.setText("회원가입이 완료되었습니다.")
         self.client_socket.send(json.dumps(list).encode())
-        incoming_message = self.client_socket.recv(1024)
-        a = json.loads(incoming_message).decode()
-        print(a)
-        # con = pymysql.connect(host='localhost', user='ElisaBluebell', password='1234',
-        #                       db='aducationapplication', charset='utf8')  # 한글처리 (charset = 'utf8')
-        #
-        # # STEP 3: Connection 으로부터 Cursor 생성
-        # cur = con.cursor()
-        #
-        # # STEP 4: SQL문 실행 및 Fetch
-        # sql = f"insert into account(class, user_name, user_id, user_password) values('', '', '{self.id_3.text()}', '{self.ps_3.text()}')"
-        # cur.execute(sql)
-        # con.commit()
-        # self.QnA_tabWidget.setCurrentIndex(0)
-    #
-    #
-    # def check_ID(self):
-    #     self.id_check = False
-    #     self.check_label.setText('중복 확인을 눌러주세요.')
-    #
-    # def join_us(self):
-    #     self.QnA_tabWidget.setCurrentIndex(1)
-    #
-    # def login(self): # 로그인 정보 전달
-    #     self.id = self.id_lineedit.text()
-    #     self.pw = self.pw_lineedit.text()
-    #     self.loginlist = [self.id, self.pw]
-    #
-    #     print(self.id)
-    #     print(self.pw)
-    #
-    #     if '' not in self.loninlist:
-    #         self.send_command('/login_teacher', self.loginlist)
-    #
-    #     if self.id == '' or self.pw == '':
-    #         self.login_label_4.setText('아이디와 비밀번호를 입력해주세요.')
-    #         return
 
-        # 서버에 전달하기위한 딕셔너리 생성
-        # login = {'id' : id, 'pw' : pw}
-        # 서버에 데이터를 전송하기 위한 send_data 함수 호출(딕셔너리 생성자)
-        # self.send_data(login, 'log')
+    def check_ID(self):
+        self.id_check = False
+        self.check_label.setText('중복 확인을 눌러주세요.')
 
-    # def join(self):  # 회원가입
-    #     print('회원가입')
-    #     if self.id_check == True:
-    #         new_user = (self.ID.text(), self.ps_3.text())
-    #         msg = f"ID: {new_user[0]}\nPW: {new_user[1]}가입하시겠습니까?"
-    #
-    #         # 가입 의사 확인 메시지박스
-    #         reply = QMessageBox.question(self, '회원가입', msg,
-    #                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-    #         if reply == QMessageBox.Yes:
-    #             # 서버에 회원가입을 위한 데이터를 전달
-    #             self.send_data(new_user, 'new')  # 튜플 new_user, 식별자 'new'
-    #             QMessageBox.information(self, '완료', '가입이 완료되었습니다.')
-    #             print('가입 완료')
-    #
-    #             # 회원가입 입력칸 초기화
-    #             self.ID.setText('')
-    #             self.ps_3.setText('')
-    #             # self.acount_tabWidget.setCurrentIndex(0)
-    #             self.id_check = False
+    def join_us(self):
+        self.tabWidget.setCurrentIndex(1)
 
-    # def send_data(self, data, idf:str): # 데이터를 전송하는 함수
-    #     # DB에 넘겨줄 데이터를 포함하는 딕셔너리 data와 식별자 idf인자로 받음
-    #     '''
-    #     log : 로그인
-    #     acc : 회원가입
-    #
-    #     '''
-    #
-    #     print('send_data -- ', end = '')
-    #     data = json.dumps(data) # 딕셔너리를 바이너리화 하는 json.dumps 메서드
-    #     print('1', end='-')
-    #     msg = idf + data # 식별자{딕셔너리 내용}
-    #     print('2', end='-')
-    #     self.client_socket.send(msg.encode()) # 바이너리를 바이트로 바꿔서 전송
-    #     print(f'{msg}전송')
+    def send(self):
+        self.answer = self.answer_lineedit.text()  # 라인에딧안에 글자를 적으면
+        pyo = self.qna_tableWidget.currentColumn()
+        rows = self.qna_tableWidget.currentRow()
+        # if self.qna_tableWidget.currentColumn() != 4:
+        if self.qna_tableWidget.currentColumn() != 4:  # 4번째 열이 아닐경우에는
+            pass  # 패스
+        else:
+            self.qna_tableWidget.setItem(rows, 4, QTableWidgetItem(self.answer))
+            send = '/answer_send', [self.answer, rows+1]
+            self.client_socket.send(json.dumps(send).encode())
 
+    def change_o(self):
+        self.a = 'O'
+        print('self.a: ', self.a)
+    def change_x(self):
+        self.a = 'X'
+        print('self.a: ', self.a)
+    def update(self):
+        quiz_up = self.quizadd_lineedit.text()  # 라인에딧안에 글자를 적으면
+        send = '/update_send', [quiz_up, self.a, self.b]
+        self.client_socket.send(json.dumps(send).encode())
+        print('성공')
 
-    # def send_chat(self):
-    #     # senders_name = self.user_name_line_edit_1p.text() #ui사용자이름
-    #     senders_name = self.user[0] # 윗줄의 코드를 수정
-    #     print(1)
-    #     data = self.signal_textEdit.toPlainText() #
-    #     print(2)
-    #     self.receive_listWidget.addItem(f'{senders_name} : {data}') #ui
-    #     print(3)
-    #     message = (f'{senders_name} : {data}').encode('utf-8')
-    #     print(4)
-    #     self.client_socket.send(message)
-    #     print(5)
-    #     self.signal_textEdit.clear()
-    #     print(6)
-        # 서버의 응답을 받는 코드
-
-    def receive_message(self, so):
+    def receive_message(self, so):  # 서버에서 메시지를(?) 받는함수
         while True:
-            buf = so.recv(9000)
+            buf = so.recv(8192)
             if not buf:  # 연결종료
                 break
 
-            # recv_data = buf.decode()  # 서버에서 응답한 데이터를 decode
-            # print(recv_data)
-            # request = recv_data[0:3]  # 서버의 응답에서 식별자 구분
-            #
-            # if len(recv_data) > 3:  # 식별자 이외의 데이터가 존재한다면 데이터 할당
-            #     self.last_request = recv_data[3:]
-            #
-            # else:  # 없다면 None (서버에서의 DB 조회 결과가 None 인 경우)
-            #     self.last_request = None
+            recv_data = buf.decode()  # 서버에서 응답한 데이터를 decode
 
+            request = eval(recv_data)  # 서버의 응답에서 식별자 구분
+            command = request[0]
+            content = request[1]
+            print(f'command: {command}')
+            print(f'content: {content}')
+            if command == '/login_success':
+                self.login_success()
+            elif command == '/login_id_fail':
+                self.login_label_4.setText('아이디와 비밀번호를 확인 해 주세요.')
+            elif command == '/login_password_fail':
+                self.login_label_4.setText('아이디와 비밀번호를 확인 해 주세요.')
+            elif command == '/location_quiz':
+                print(content[0][1])
+                # self.qt_textEdit.setPlainText("content[0][1]") ###
+                # self.quiz_textBrowser
+                # self.quiz_tableWidget.setItem(0, 0, QTableWidgetItem(content[0][1])) # 오류는 안나는데 테이블위젯에 안나타남
+                self.quiz_label.setText(content[0][1])
+                self.quiz_label.setWordWrap(True)  # 자동 줄바꿈
+                print(12123123)
+                # print(self.quiz_textBrowser)
+            elif command == '/whole_qna_data':
+                for i in range(len(content)):
+                    for j in range(len(content[i])):
+                        content[i][j] = str(content[i][j])
+                        content[i][j] = content[i][j].replace('\n', ' ')
+                        self.qna_tableWidget.setItem(i, j, QTableWidgetItem(content[i][j]))
+                        # self.qna_tableWidget.setFixedSize(1000, 2000) # 테이블 위젯 사이즈
+                        self.qna_tableWidget.horizontalHeader().setVisible(True)  # 칼럼헤더 보이기
+                        table = self.qna_tableWidget  # 내용에 따라 열너비 자동조정
+                        header = table.horizontalHeader()  # 내용에 따라 열너비 자동조정
+                        header.setSectionResizeMode(0, QHeaderView.ResizeToContents)  # 내용에 따라 열너비 자동조정
+                        header.setSectionResizeMode(1, QHeaderView.ResizeToContents)  # 내용에 따라 열너비 자동조정
+                        header.setSectionResizeMode(2, QHeaderView.ResizeToContents)  # 내용에 따라 열너비 자동조정
+                        header.setSectionResizeMode(3, QHeaderView.ResizeToContents)  # 내용에 따라 열너비 자동조정
+                        header.setSectionResizeMode(4, QHeaderView.ResizeToContents)  # 내용에 따라 열너비 자동조정
+            elif command == '/register_fail':
+                self.join_label.setText("이미 등록되어 있는 회원입니다.")
+            elif command == '/quiz_inserted':
+                self.update_label.setText("문제 업데이트 완료")
+            elif command == '/student_score':
+                for i in range(len(content)):
+                    self.score_tableWidget.setItem(i, 0, QTableWidgetItem(str(int(content[[0][1][0]+1[0]))) # 행열 QTableWidgetItem(str(content[0][1][8][0]
+                    # self.score_tableWidget.setItem(i, 0, QTableWidgetItem(str(content[0][2])))
+
+                    # send = '/answer_send', [self.answer, content[0]]
+
+
+        # self.answer = self.answer_lineedit.text()  # 라인에딧안에 글자를 적으면
+        # pyo = self.score_tableWidget.currentColumn()
+        # rows = self.score_tableWidget.currentRow()
+        # # if self.qna_tableWidget.currentColumn() != 4:
+        # if self.qna_tableWidget.currentColumn() != 4:  # 4번째 열이 아닐경우에는
+        #     pass  # 패스
+        # else:
+        #     self.score_tableWidget.setItem(rows, 4, QTableWidgetItem(self.answer))
+        #     send = '/answer_send', [self.answer, rows+1]
+        #     self.client_socket.send(json.dumps(send).encode())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
